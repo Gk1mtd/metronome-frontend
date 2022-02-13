7f51780813b1841d11ade06968509bc1557abd95
Philipp Kastl <picastle@posteo.de>
2022-02-13 13:11:38 +0100

    add error route for not allowed paths

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Setlists from './components/Setlists';
+import Error from './components/Error';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Home />
-
       <Routes>
+        <Route exact path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
         <Route path='/setlists' element={<Setlists />} />
+        <Route path='/*' element={<Error />} />
       </Routes>
     </div>
   );
---
+++ src/components/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Error() {
+  return <div className='Error'>Error</div>;
+}
+
+export default Error;
