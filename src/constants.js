b875873a15380077f1479a0e99ec5d91dbd27b72
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-19 09:31:48 +0100

    substituted by dotenv

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * easy to import hardcoded paths, used across the whole project
- */
-export const constants = {
-  baseUrl: 'https://metroknowme.herokuapp.com/api',
-  baseUrl_local: 'http://localhost:4000/api',
-};
