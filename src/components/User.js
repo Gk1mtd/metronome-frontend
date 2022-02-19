b747e7afd70d3fb7b636816ed043c34c0b8493a9
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-19 13:19:21 +0100

    retrieves now email instead of id

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import useAPI from '../customHooks/api';
+import useAPI from '../customHooks/user.api';
+import {Link} from 'react-router-dom'
 
 /** provides logout and delteuser through custom hook api
  * shows currently logged in user
@@ -7,12 +8,12 @@ import useAPI from '../customHooks/api';
 function User() {
   const { logout, deleteUser } = useAPI();
 
-  const user = localStorage.getItem('user');
+  const email = localStorage.getItem('email');
 
   return (
     <div>
-      User
-      {<p>local user is: {user}</p>}
+      <Link to='/setlists'>back to Setlists</Link>
+      <p>local user is: </p> {email}
       <button onClick={logout}>logout</button>
       <button onClick={deleteUser}>delete account</button>
     </div>
