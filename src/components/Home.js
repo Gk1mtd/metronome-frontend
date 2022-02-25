7f1f1f496a39bbaffd3607ac15cb066679cccc22
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-25 17:53:25 +0100

    adds a moving logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from "../assets/logo.webm"
 
 function Home() {
   return (
     <div className='Home'>
       <h1>Home</h1>
-      <img src='https://metroknowme.netlify.app/logo512.png' alt='logo2' />
+      <video autoPlay={true} width={200} loop={true} src={logo} alt='logo2' />
+      <br/>
       <Link to='/signup'>Signup</Link>
       <Link to='/login'>Login</Link>
     </div>
